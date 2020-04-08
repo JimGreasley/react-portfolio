e9a6aff1fddb479be815c481678e1109cdbd1cf3
Jim Greasley <azjimgreasley@gmail.com>
2020-04-08 16:44:07 -0700

    Updates

---
+++ precache-manifest.fec7e57a184475899c9fb375881cc90e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7da9aee5b199f301021651384c5eeb0",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "b262918464693b51a103",
+    "url": "/react-portfolio/static/css/main.540b9ef1.chunk.css"
+  },
+  {
+    "revision": "43cc0a43988368035016",
+    "url": "/react-portfolio/static/js/2.513ef1cf.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-portfolio/static/js/2.513ef1cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b262918464693b51a103",
+    "url": "/react-portfolio/static/js/main.1ceae942.chunk.js"
+  },
+  {
+    "revision": "0346f5e54be923396aed",
+    "url": "/react-portfolio/static/js/runtime-main.8a7d631b.js"
+  }
+]);
